a793f32be7fc2741b05c80a402d0c8fa69dc64eb
Jef Van den broeck <jef@codewerken.be>
2018-04-05 01:49:33 +0200

    add a script to load up a MySQL shell

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -4,6 +4,6 @@ OUT=`date --iso-8601=seconds`.sql
 DDIR=`dirname $0`/../
 
 . $DDIR/env
-docker-compose -f $DDIR/docker-compose.yml  exec db mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --all-databases --single-transaction --result-file=/backups/$OUT
+docker-compose -f $DDIR/docker-compose.yml exec db mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --all-databases --single-transaction --result-file=/backups/$OUT
 
 echo `readlink -e $DDIR/db/backups/$OUT`
